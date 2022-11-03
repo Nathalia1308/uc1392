271d9ee5961b92d74e1c572f0c141afa899ddd6d
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-11-03 11:56:54 -0300

    Alterações feitas

---
+++ aula.php
@@ -0,0 +1,31 @@
+<?php
+    $premio = 1547.90;
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 2 - UC13 </title>
+</head>
+<body>
+    <table>
+        <th>id</th>
+        <th>nome</th>
+        <th>telefone</th>
+        <th>prêmio</th>
+        <tr>
+            <td>1555</td>
+            <td>Nathalia</td>
+            <td>2158-9200</td>
+            <td><?php echo($premio * 0.12);?></td>
+        </tr>
+        <tr>
+            <td>1498</td>
+            <td>Mayara</td>
+            <td>2185-9231</td>
+            <td><?php echo($premio * 0.28);?></td>
+        </tr>
+    </table>
+</body>
+</html>
