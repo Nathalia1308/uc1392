382179abc016188a9e5a8cfe16ae9e14ba66538d
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-11-09 11:55:13 -0300

    manipulando datas e funções de sistemas

--- aula4.php
+++ aula4.php
@@ -1,52 +1,70 @@
-<?php 
-    include ('config.php');
-    include ('funcoes.php');
+<?php
+include('config.php');
+include('funcoes.php');
 
-    $parcelas = array();
-    $coeficiente = 0.0;
-
-    if(isset($_POST['calcular'])){
-        $capital = $_POST['capital'];
-        $taxa = $_POST['taxa'];
-        $parcela = $_POST['parcela'];
-        $coeficiente = parcelar(floatval($taxa), intval($parcela));
-
-        $data = date('d/m/Y');
-        $dias = 30;
-
-        for($i=0; $i < $parcela; $i++){
-            $parcelas = ([$i+1, '.ª' ($coeficiente*floatval($capital)), date($data,strtotime("+.$dias"))]);
-        }
-        
+$parcelas = array();
+$coeficiente = 0.0;
+if(isset($_POST['calcular']))
+{
+    $capital = $_POST['capital'];
+    $taxa    = $_POST['taxa'];
+    $parcela = $_POST['parcela'];
+    $coeficiente = parcelar(floatval($taxa), intval($parcela));
+    $montante = 0;
+   // $data = date('d/m/Y');
+    $dias = 30;
+    for ($i=0; $i < $parcela; $i++) { 
+        $parcelas[$i] = [($i+1).'ª',($coeficiente*floatval($capital)),new DateTime('+'.$dias. 'days')];    
+        $dias += 30; //$dias = $dias + 30;
+        $montante += $coeficiente*floatval($capital);
     }
+    //print_r($parcelas);
+}
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Parcelamento</title>
 </head>
 <body>
-    
+
     <form action="#" method="post">
-        <input type="text" name="capital" placeholder="Capital..."><br>
+        <input type="text" name="capital" placeholder="Capital (R$)..."><br>
         <input type="text" name="taxa" placeholder="Taxa (%)..."><br>
         <input type="text" name="parcela" placeholder="Parcelas (1)..."><br>
         <button type="submit" name="calcular">Calcular</button>
     </form>
     <br><hr>
+
+    <?php if(count($parcelas)>0){?>
+        <h4>Valor da Capital: R$ <?php echo number_format($capital,2,',','.'); ?></h4>
+        <h4>Taxa de juro: <?php echo $taxa; ?> %</h4>
+        <h4>Parcelas: <?php echo $parcela; ?> meses</h4>
+        <h4>Total do financiamento: R$ <?php echo number_format($montante,2,',','.'); ?></h4>
+    <table class="tabelinha">
+        <th>#</th>
+        <th>Valor</th>
+        <th>Vencimento</th>
+
     <?php 
-        if(count($parcelas)>0){
-    ?>        
-        <h3>Valor da parcela: R$ <?php  echo $parcelas[3];?></h3>
-        <h3>Taxa de juros: </h3>
-        <h3>Parcelas: </h3>
-    <?php 
+        foreach ($parcelas as $valores) {
+            ?>
+        <tr>    
+            <td><?php echo($valores[0]);?>
+            <td><?php echo number_format($valores[1],2)." - ";?></td>
+            <td><?php echo $valores[2]->format('d-m-Y');?></td>
+        </tr>
+            <?php
         }
-    ?>
+            ?>
+        </table>
+        <?php     
+    } ?>
+
 </body>
 </html>
--- funcoes.php
+++ funcoes.php
@@ -1,7 +1,12 @@
 <?php 
-function parcelar(float $taxa, int $parcelas=1):float 
-{
+function parcelar(float $taxa, int $parcelas=1):float {
     $coeficiente = pow((1 + ($taxa/100)), $parcelas) /$parcelas;
     return $coeficiente; // parcelas fixas
+}
+
+function dataTexto(DateTime $data){
+    $intervalo = $data->diff(new DateTime());
+    return $intervalo->format('%y anos, %m meses, %d dias');
+
 }
 ?>
