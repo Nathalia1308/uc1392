0cab77672ebaf06fcd26ff0e457718e1229c42f0
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-11-16 12:00:37 -0300

    conectando banco de dados e php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     date_default_timezone_set('America/Sao_Paulo');
 
     const ID = "HPKR525KJ745";
-    const DATABASE = "uc1392db";
+    const DATABASE = "db_locadora";
     const SERVER_DB = "127.0.0.1";
     const SENHA_DB = "";
     const USER_DB = "root";
