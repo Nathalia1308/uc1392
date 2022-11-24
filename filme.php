11eb84d9525193795d153cc3c8aa4a02ea26f11e
Nathalia Barros da Silva <nathysilva.1502@gmail.com>
2022-11-24 09:16:23 -0300

    alterações realizadas no dia 23/11/2022

--- filme.php
+++ filme.php
@@ -3,17 +3,17 @@
 include 'conecta.php';
 
 // cria a consulta sql
-$consultaSql = "SELECT * FROM filme order by titulo asc";
+$consultaSql = "SELECT * FROM vw_filme_class order by titulo asc";
 
 // trazer a lista completa dos dados 
 $lista = $pdo->query("$consultaSql");
-
+$listaClass = $pdo->query("select cod_classificacao as id, classificacoes as class from classificacao");
 // separar os dados em linhas
 
 // $linha = $lista->fetch();
 
 $row = $lista->fetch();
-
+$rowClass = $listaClass->fetch();
 // retornando o número de linhas
 
 // $num_linhas = $lista->rowCount();
@@ -23,13 +23,13 @@ $num_rows = $lista->rowCount();
 if(isset($_POST['enviar'])){
     $titulo = $_POST['titulo'];
     $sinopse = $_POST['sinopse'];
-    $genero = $_POST['genero'];
+    // $genero = $_POST['genero'];
     $lancamento = $_POST['lancamento'];
     $origem = $_POST['origem'];
+    $duracao = $_POST['duracao'];
     $preco = $_POST['preco'];
-    $classificacao = $_POST['classificacao'];
-
-    $consulta = "inset filme (titulo, sinopse, genero, lancamento, origem, duracao, preco, classificacao) values ('$titulo','$sinopse','$genero','$lancamento','$origem','$duracao','$preco','$classificacao')";
+    $cod_classificacao = $_POST['class'];
+    $consulta = "insert filme (titulo, sinopse, lancamento, pais_origem, duracao, preco, cod_classificacao) values ('$titulo','$sinopse','$lancamento','$duracao','$duracao','$preco','$cod_classificacao')";
     $resultado = $pdo->query($consulta);
     $_POST['enviar'] = null;
     header('location: filme.php');
@@ -55,72 +55,86 @@ if(isset($_POST['enviar'])){
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmes <?php echo '(' .$num_rows.')' ?></title>
 
 </head>
 <body>
     <section class="formulario">
-        <form action="#" method="POST"></form>
+        <form action="#" method="POST">
         <div hidden>
             <label for="cod-filme">
                 Código
-                <input type="text" name="cod-filme">
+                <input class="form-control" type="text" name="cod-filme">
             </label>
         </div>
         <div>
             <label for="titulo">
                 Título
-                <input type="text" name="titulo" required>
+                <input class="form-control" type="text" name="titulo" required>
             </label>
         </div>
         <div>
             <label for="sinopse">
                 Sinopse
-                <input type="text" name="sinopse" required>
+                <input class="form-control" type="text" name="sinopse" required>
             </label>
         </div>
-        <div>
+        <!-- <div>
             <label for="genero">
                 Genero
-                <input type="text" name="genero" required>
+                <input class="form-control" type="text" name="genero" required>
             </label>
-        </div>
+        </div> -->
         <div>
             <label for="lancamento">
                 Lançamento
-                <input type="text" name="lancamento" required>
+                <input class="form-control" type="text" name="lancamento" required>
             </label>
         </div>
         <div>
             <label for="origem">
                 Origem
-                <input type="text" name="origem" required>
+                <input class="form-control" type="text" name="origem" required>
+            </label>
+        </div>
+        <div>
+            <label for="duracao">
+                Duração
+                <input class="form-control" type="text" name="duracao" required>
             </label>
         </div>
         <div>
             <label for="preco">
                 Preço 
-                <input type="text" name="preco" required>
+                <input class="form-control" type="text" name="preco" required>
             </label>
         </div>
+
         <div>
-            <label for="classificacao">
-                Classificação
-                <input type="text" name="classificacacao" required>
+             <label for="Classificacao">
+                 Classificação
+            <select class="form-control" name="class" id="">
+                <?php do { ?>
+                <option value="<?php echo $rowClass['id']?>"><?php echo $rowClass['class']?></option>
+                 <?php } while($rowClass = $listaClass->fetch());?>
+            </select>
             </label>
         </div>
+        
         <div>
-            <button type="submit" name="enviar" id="btn-enviar">Enviar</button>
+        <button type="submit" name="enviar" id="" class="btn btn-secondary">Enviar</button>
         </div>
+        </form>
     </section>
-    <table>
+    <table class="table table-striped table-hover">
         <thead>
             <th hidden>ID</th>
             <th>Título</th>
             <th>Sinopse</th>
-            <th>Genero</th>
+            <!-- <th>Genero</th> -->
             <th>Lançamento</th>
             <th>Origem</th>
             <th>Duração</th>
@@ -133,15 +147,16 @@ if(isset($_POST['enviar'])){
                     <td hidden><?php echo $row['cod_filme'];?></td>
                     <td><?php echo $row['titulo'];?></td>
                     <td><?php echo $row['sinopse'];?></td>
-                    <td><?php echo $row['genero'];?></td>
+                    <!-- <td><?php echo $row['genero'];?></td> -->
                     <td><?php echo $row['lancamento'];?></td>
-                    <td><?php echo $row['origem'];?></td>
+                    <td><?php echo $row['pais_origem'];?></td>
                     <td><?php echo $row['duracao'];?></td>
                     <td><?php echo $row['preco'];?></td>
-                    <td><?php echo $row['classificacoes'];?></td>
+                    <td><?php echo $row['classificacao'];?></td>
                 </tr>
             <?php }while($row = $lista->fetch());?>
         </tbody>
     </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
